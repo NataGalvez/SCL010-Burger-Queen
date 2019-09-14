ec248834918c9c590ad4be9ba997e4b9aa2e8f6e
NataGalvez <nataliac.galvezc@gmail.com>
2019-09-14 10:32:50 -0300

    arreglo nombre carpete

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const express = require('express');
 const path = require('path');
 const app = express();
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/burger-queen'));
+app.use(express.static('./dist/BurgerQueen'));
 app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname,'/dist/burger-queen/index.html'));
+res.sendFile(path.join(__dirname,'/dist/BurgerQueen/index.html'));
 });
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
