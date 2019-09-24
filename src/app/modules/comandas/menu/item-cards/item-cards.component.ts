72a693df36462dec02d7fce5d041fa921f45d8eb
NataGalvez <nataliac.galvezc@gmail.com>
2019-09-24 16:22:52 -0300

    Se muestra menu.

--- src/app/modules/comandas/menu/item-cards/item-cards.component.ts
+++ src/app/modules/comandas/menu/item-cards/item-cards.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit } from "@angular/core";
+import productos from "../../../../models/productos.json";
 @Component({
-  selector: 'app-item-cards',
-  templateUrl: './item-cards.component.html',
-  styleUrls: ['./item-cards.component.css']
+  selector: "app-item-cards",
+  templateUrl: "./item-cards.component.html",
+  styleUrls: ["./item-cards.component.css"]
 })
 export class ItemCardsComponent implements OnInit {
+  products = productos;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
