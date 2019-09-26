78d44e80697da96d92330402483bed84365bba25
NataGalvez <nataliac.galvezc@gmail.com>
2019-09-26 19:47:56 -0300

    Se modifica orden por orders

--- src/app/modules/comandas/menu/item-cards/item-cards.component.ts
+++ src/app/modules/comandas/menu/item-cards/item-cards.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { CocinaService } from "src/app/services/cocina.service";
+import { ActivatedRoute } from "@angular/router";
+import { MesasService } from "src/app/services/mesas.service";
 
 @Component({
   selector: "app-item-cards",
@@ -7,10 +9,23 @@ import { CocinaService } from "src/app/services/cocina.service";
   styleUrls: ["./item-cards.component.css"]
 })
 export class ItemCardsComponent implements OnInit {
+  tableNumber: number;
   products: any[];
-  constructor(private cocinaService: CocinaService) {}
+
+  constructor(
+    private cocinaService: CocinaService,
+    private route: ActivatedRoute,
+    private mesasService: MesasService
+  ) {}
 
   ngOnInit() {
+    this.route.params.subscribe(params => {
+      this.tableNumber = +params.tableNumber;
+    });
     this.products = this.cocinaService.getProducts();
+  }
+  addProducts(product) {
+    this.mesasService.addProductToOrder(this.tableNumber, product);
+    console.log(this.tableNumber);
   }
 }
--- src/app/services/mesas.service.ts
+++ src/app/services/mesas.service.ts
@@ -6,7 +6,28 @@ import mesas from "../models/mesas.json";
 })
 export class MesasService {
   private tables: any[] = mesas;
+
   constructor() {}
+  // Añade los productos a la orden en el numero de mesa correspondiente
+  addProductToOrder(tableNumber, product): any {
+    this.tables.forEach(table => {
+      if (table.tableNumber === tableNumber) {
+        console.log(table);
+        table.orders.push(product);
+      }
+    });
+  }
+
+  getOrderFromTables(tableNumber): any[] {
+    let output = [];
+    this.tables.forEach(table => {
+      if (table.tableNumber === tableNumber) {
+        output = table.orders;
+      }
+    });
+    return output;
+  }
+
   getTables() {
     return this.tables;
   }
