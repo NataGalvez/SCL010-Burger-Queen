f271a7192246a9dbdd6017a89dc6897b800bdb80
Natalia Gálvez Caroca <51415899+NataGalvez@users.noreply.github.com>
2019-09-27 08:26:48 -0300

    Feature/nata galvez/menu select (#12)
    * Se muestra menu.
    
    * Estilos pantalla Mesas
    
    * Se añade propiedad "turn" para diferenciar menu desayuno y almuerzo
    
    * update order component
    
    * routing dynamic
    
    * Add two tables at Json.
    
    * conectamos ssevicios a componentes mesas y cartitas
    
    * Se modifica orden por orders
    
    * Se crea función getQuantity y checkifProductExist

--- src/app/modules/comandas/menu/item-cards/item-cards.component.ts
+++ src/app/modules/comandas/menu/item-cards/item-cards.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { CocinaService } from "src/app/services/cocina.service";
+import { ActivatedRoute } from "@angular/router";
+import { MesasService } from "src/app/services/mesas.service";
 
 @Component({
-  selector: 'app-item-cards',
-  templateUrl: './item-cards.component.html',
-  styleUrls: ['./item-cards.component.css']
+  selector: "app-item-cards",
+  templateUrl: "./item-cards.component.html",
+  styleUrls: ["./item-cards.component.css"]
 })
 export class ItemCardsComponent implements OnInit {
+  tableNumber: number;
+  products: any[];
 
-  constructor() { }
+  constructor(
+    private cocinaService: CocinaService,
+    private route: ActivatedRoute,
+    private mesasService: MesasService
+  ) {}
 
   ngOnInit() {
+    this.route.params.subscribe(params => {
+      this.tableNumber = +params.tableNumber;
+    });
+    this.products = this.cocinaService.getProducts();
   }
-
+  addProducts(product) {
+    this.mesasService.addProductToOrder(this.tableNumber, product);
+  }
 }
--- src/app/modules/comandas/table/table.component.ts
+++ src/app/modules/comandas/table/table.component.ts
@@ -1,12 +1,25 @@
 import { Component, OnInit } from "@angular/core";
-
+import { MesasService } from "../../../services/mesas.service";
 @Component({
   selector: "app-table",
   templateUrl: "./table.component.html",
   styleUrls: ["./table.component.css"]
 })
 export class TableComponent implements OnInit {
-  constructor() {}
+  tables: any[];
+  type: string;
+  time: Date;
+  constructor(private mesasService: MesasService) {
+    this.time = new Date();
+  }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.tables = this.mesasService.getTables();
+    if (this.time.getHours() >= 6 && this.time.getHours() <= 12) {
+      this.type = "desayuno";
+    } else {
+      this.type = "almuerzo";
+    }
+    console.log(this.time);
+  }
 }
